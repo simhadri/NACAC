08e60455d36cd8c2cf3d9d21e409ecc7abf0ae4d
Peter Grillot <petergrillot@gmail.com>
2016-04-14 09:28:36 -0400

    Remove Animate CSS

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -11,8 +11,3 @@ import 'modules/blazySettings';
 import 'modules/navigation';
 import 'modules/search';
 import 'modules/tabs';
-
-// SUPER LAME FADE IN
-$(document).ready(function() {
-	$('main').addClass("fadeIn");
-});
