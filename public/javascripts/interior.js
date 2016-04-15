febe716fccc9de17f5c945f67c29d687caefd49e
Peter Grillot <petergrillot@gmail.com>
2016-04-15 15:44:03 -0400

    Timer is Done

--- public/javascripts/interior.js
+++ public/javascripts/interior.js
@@ -1,27 +1,61 @@
-webpackJsonp([1],{
-
-/***/ 0:
+webpackJsonp([2,4],[
+/* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	module.exports = __webpack_require__(9);
+	module.exports = __webpack_require__(12);
 
 
 /***/ },
+/* 1 */,
+/* 2 */,
+/* 3 */,
+/* 4 */,
+/* 5 */
+/***/ function(module, exports) {
 
-/***/ 9:
-/***/ function(module, exports, __webpack_require__) {
-
-	// *********************
-	//    Modules scripts
-	// *********************
+	"use strict";
 	
-	'use strict';
-	
-	__webpack_require__(10);
+	var Throttled = function Throttled(func, wait, options) {
+		var now = Date.now || function () {
+			return new Date().getTime();
+		};
+		var context, args, result;
+		var timeout = null;
+		var previous = 0;
+		if (!options) options = {};
+		var later = function later() {
+			previous = options.leading === false ? 0 : now();
+			timeout = null;
+			result = func.apply(context, args);
+			if (!timeout) context = args = null;
+		};
+		return function () {
+			if (!previous && options.leading === false) previous = now();
+			var remaining = wait - (now() - previous);
+			context = this;
+			args = arguments;
+			if (remaining <= 0 || remaining > wait) {
+				if (timeout) {
+					clearTimeout(timeout);
+					timeout = null;
+				}
+				previous = now();
+				result = func.apply(context, args);
+				if (!timeout) context = args = null;
+			} else if (!timeout && options.trailing !== false) {
+				timeout = setTimeout(later, remaining);
+			}
+			return result;
+		};
+	};
+	module.exports = Throttled;
 
 /***/ },
-
-/***/ 10:
+/* 6 */,
+/* 7 */,
+/* 8 */,
+/* 9 */,
+/* 10 */
 /***/ function(module, exports, __webpack_require__) {
 
 	// MODULES
@@ -42,14 +76,17 @@ webpackJsonp([1],{
 			    asideNavigationContainerHeight = $(".aside-navigation__row").height(),
 			    asideNavigationHeight = asideNavigation.height(),
 			   
+			// 170 off set
+			interiorHeroHeight = $(".hero__wrapper").height() + $(".interior-hero__breadcrumb").height() + 170,
+			   
 			// 40 is for its margins
 			asideNavigationLocation = asideNavigation.offset().top - 40,
 			    offsetOfFirstArticle = $('.article__row:first-child').offset();
 	
-			if (bottomOfNavigation > $(".interior-hero__wrapper").height() + $(".interior-hero__breadcrumb").height() + 70) {
+			if (bottomOfNavigation > interiorHeroHeight) {
 				$('.aside-navigation').addClass('aside-navigation--sticky');
 			}
-			if (bottomOfNavigation < 610) {
+			if (bottomOfNavigation < interiorHeroHeight) {
 				$('.aside-navigation').removeClass('aside-navigation--sticky');
 			}
 			// if location of aside-nav less its size
@@ -64,7 +101,19 @@ webpackJsonp([1],{
 		$(window).scroll((0, _modulesThrottledJs2["default"])(asideNavigation, 20));
 	})();
 
-/***/ }
+/***/ },
+/* 11 */,
+/* 12 */
+/***/ function(module, exports, __webpack_require__) {
 
-});
+	// *********************
+	//    Modules scripts
+	// *********************
+	
+	'use strict';
+	
+	__webpack_require__(10);
+
+/***/ }
+]);
 //# sourceMappingURL=interior.js.map
--- public/javascripts/numberCount.js
+++ public/javascripts/numberCount.js
@@ -1,14 +1,56 @@
-webpackJsonp([2],{
+webpackJsonp([3,4],{
 
 /***/ 0:
 /***/ function(module, exports, __webpack_require__) {
 
-	module.exports = __webpack_require__(11);
+	module.exports = __webpack_require__(13);
 
 
 /***/ },
 
-/***/ 11:
+/***/ 5:
+/***/ function(module, exports) {
+
+	"use strict";
+	
+	var Throttled = function Throttled(func, wait, options) {
+		var now = Date.now || function () {
+			return new Date().getTime();
+		};
+		var context, args, result;
+		var timeout = null;
+		var previous = 0;
+		if (!options) options = {};
+		var later = function later() {
+			previous = options.leading === false ? 0 : now();
+			timeout = null;
+			result = func.apply(context, args);
+			if (!timeout) context = args = null;
+		};
+		return function () {
+			if (!previous && options.leading === false) previous = now();
+			var remaining = wait - (now() - previous);
+			context = this;
+			args = arguments;
+			if (remaining <= 0 || remaining > wait) {
+				if (timeout) {
+					clearTimeout(timeout);
+					timeout = null;
+				}
+				previous = now();
+				result = func.apply(context, args);
+				if (!timeout) context = args = null;
+			} else if (!timeout && options.trailing !== false) {
+				timeout = setTimeout(later, remaining);
+			}
+			return result;
+		};
+	};
+	module.exports = Throttled;
+
+/***/ },
+
+/***/ 13:
 /***/ function(module, exports, __webpack_require__) {
 
 	// *********************
@@ -18,7 +60,7 @@ webpackJsonp([2],{
 	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 	
-	var _vendorCountUpJs = __webpack_require__(12);
+	var _vendorCountUpJs = __webpack_require__(14);
 	
 	var _vendorCountUpJs2 = _interopRequireDefault(_vendorCountUpJs);
 	
@@ -42,7 +84,7 @@ webpackJsonp([2],{
 
 /***/ },
 
-/***/ 12:
+/***/ 14:
 /***/ function(module, exports) {
 
 	/*
