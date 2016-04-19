55c4a2b7f8412d76728a85c34dde9bd68105c3da
Peter Grillot <petergrillot@gmail.com>
2016-04-19 10:26:22 -0400

    Clean up templating; nav tweak

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -188,7 +188,7 @@ webpackJsonp([0,4],[
 			// If past than util nav and animation NOT fired
 			if (bodyTop >= browserViewport - utilityHeight && !primaryNav.hasClass('primary-nav--up')) {
 				headStyle.removeRules();
-				primaryNav.addClass('primary-nav--up primary-nav--sticky no-transitions');
+				primaryNav.addClass('primary-nav--up primary-nav--sticky primary-nav--inanimate');
 				$('main').css({ 'padding-top': '7rem' });
 			}
 			//If NOT past util nav, unstick
