5e6bbac7e24e5cf00be41094447acf0dac6d2760
Peter Grillot <petergrillot@gmail.com>
2016-05-03 15:09:36 -0400

    added close accordion

--- src/javascripts/modules/accordion.js
+++ src/javascripts/modules/accordion.js
@@ -1,11 +1,17 @@
 (function() {
 	'use strict';
 	var accordionItem = $('.accordion__item'),
-		accordionTrigger = $('.accordion-item__header'),
-		openaccordion = function() {
-			accordionItem.removeClass('accordion__item--selected');
-			$(this).parent().addClass('accordion__item--selected');
+		accordionTrigger = $('.accordion-item__trigger'),
+		isOpen = false,
+		toggleAccordion = function(evt) {		
+			if ($(this).hasClass('accordion__item--selected')) {
+				$(this).removeClass('accordion__item--selected');
+				console.log('thing')
+			} else {
+				accordionItem.removeClass('accordion__item--selected');
+				$(this).addClass('accordion__item--selected');
+			}
 		};
 	// events
-	accordionTrigger.on('click', openaccordion);
+	accordionItem.on('click', toggleAccordion);
 })();
