5febf34fd58f997ca24a8b9c317345c586fdaa8d
Peter Grillot <petergrillot@gmail.com>
2016-05-17 18:05:17 -0400

    formValidation

--- src/javascripts/modules/formValidation.js
+++ src/javascripts/modules/formValidation.js
@@ -1,28 +1,30 @@
-var validator = new FormValidator('productForm', [{
-	name: 'email',
-	rules: 'valid_email'
-}, {
-	names: ['firstName', 'lastName'],
-	rules: 'required|alpha'
-}], function(errors, evt) {
+if (document.getElementById('productForm')){
+	var validator = new FormValidator('productForm', [{
+		name: 'email',
+		rules: 'valid_email'
+	}, {
+		names: ['firstName', 'lastName'],
+		rules: 'required|alpha'
+	}], function(errors, evt) {
 
-	if (errors.length > 0) {
+		if (errors.length > 0) {
 
-		// Show the errors
-		// console.log(this)
-		
-		for (var i = 0; i < this.form.length; i++) {
+			// Show the errors
+			// console.log(this)
 			
-			if (this.form[i].classList.contains('required')){
-				// let err = document.createElement('span');
-				// err.id = 'error-message_' + i;
-				// err.className = 'error-message';
-				// err.innerHTML = 'This field is required!';
-				console.log(this.form[i].nextSibling)
-				this.form[i].nextSibling.innerHTML = 'pr';
-				this.form[i].classList.add('error');
+			for (var i = 0; i < this.form.length; i++) {
+				
+				if (this.form[i].classList.contains('required')){
+					// let err = document.createElement('span');
+					// err.id = 'error-message_' + i;
+					// err.className = 'error-message';
+					// err.innerHTML = 'This field is required!';
+					console.log(this.form[i].nextSibling)
+					this.form[i].nextSibling.innerHTML = 'pr';
+					this.form[i].classList.add('error');
+				}
 			}
+			return false;
 		}
-		return false;
-	}
-});
+	});
+}
