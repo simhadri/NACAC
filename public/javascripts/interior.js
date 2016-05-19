8cefab0ead7155feb100699eb28cc1c7c2d6d0bb
Peter Grillot <petergrillot@gmail.com>
2016-05-19 16:02:58 -0400

    improve validation

--- public/javascripts/interior.js
+++ public/javascripts/interior.js
@@ -195,24 +195,25 @@ webpackJsonp([3],[
 				display: 'email',
 				rules: 'required|valid_email'
 			}], function (errors, evt) {
-	
+				// console.log(this.fields.lastName.element)
+				for (var field in this.fields) {
+					console.log("obj." + field, this.fields[field].element.validity.valid);
+					if (this.fields[field].element.validity.valid) {
+						this.fields[field].element.classList.remove('error');
+					} else {
+						this.fields[field].element.classList.add('error');
+					}
+					// this.fields[field].isValid;
+				}
 				if (errors.length > 0) {
 					var errBox = document.getElementById('error-box');
 					errBox.innerHTML = '';
-					console.log(errors[0].messages);
+	
 					for (var n = 0; n < errors[0].messages.length; n++) {
 						// errors[0].messages[n];
-						console.log(errBox);
 						errBox.innerHTML = errBox.innerHTML + errors[0].messages[n] + '<br>';
 					}
-					for (var i = 0; i < this.form.length; i++) {
-	
-						if (this.form[i].classList.contains('required')) {
-							// var errMessage = this.form[i].nextSibling.nextSibling;
-							// errMessage.innerHTML = 'This Field is Required';
-							this.form[i].classList.add('error');
-						}
-					}
+					errBox.classList.add('error-box--display');
 					return false;
 				}
 			});
