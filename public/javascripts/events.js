ec1099cc69e3c37f0d8a66f846c6aebdeafcbf95
Peter Grillot <petergrillot@gmail.com>
2016-06-07 10:16:58 -0400

    Fix up slider, ul ol, content detail

--- public/javascripts/events.js
+++ public/javascripts/events.js
@@ -168,28 +168,30 @@ webpackJsonp([2],[
 	// FUNCTION
 	'use strict';
 	
-	$('#lightSlider').lightSlider({
-		item: 4,
-		loop: false,
-		slideMove: 4,
-		pager: false,
-		easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
-		speed: 600,
-		responsive: [{
-			breakpoint: 800,
-			settings: {
-				item: 3,
-				slideMove: 1,
-				slideMargin: 6
-			}
-		}, {
-			breakpoint: 480,
-			settings: {
-				item: 2,
-				slideMove: 1
-			}
-		}]
-	});
+	if (document.getElementById('lightSlider')) {
+		$('#lightSlider').lightSlider({
+			item: 4,
+			loop: false,
+			slideMove: 4,
+			pager: false,
+			easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
+			speed: 600,
+			responsive: [{
+				breakpoint: 800,
+				settings: {
+					item: 3,
+					slideMove: 1,
+					slideMargin: 6
+				}
+			}, {
+				breakpoint: 480,
+				settings: {
+					item: 2,
+					slideMove: 1
+				}
+			}]
+		});
+	}
 
 /***/ }
 ]);
