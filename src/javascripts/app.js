2f607f7547924ec1ed2d6a05a8c4c874cfcd548e
Peter Grillot <petergrillot@gmail.com>
2016-07-11 12:07:16 -0400

    Success: config now declares env variable.
    We can now create if for ‘env’ inside app.js. Need to test for its use
    in modules
    ```
    if(process.env.NODE_ENV === 'development')
    ```

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -1,6 +1,18 @@
 // *********************
 //    Modules scripts
 // *********************
+
+// USING production variables
+// if(process.env.NODE_ENV === 'development'){
+// 	var dev_var = 'url/to/dev';
+	
+// }
+// if(process.env.NODE_ENV === 'production'){
+// 	var dev_var = 'url/to/prod';
+	
+// }
+// console.log(dev_var)
+
 import 'modules/episerverViewMode';
 import 'modules/blazySettings';
 import 'modules/navigation';
