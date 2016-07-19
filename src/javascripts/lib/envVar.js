f72616d454eafb54bfeb677a8c2c4b70f39b3383
Peter Grillot <petergrillot@gmail.com>
2016-07-19 13:40:05 -0400

    improved envier

--- src/javascripts/lib/envVar.js
+++ src/javascripts/lib/envVar.js
@@ -1,13 +1,16 @@
 // Jquery adds inline styles and these need to be overwritten. 
 // HeadStyle writes styles to the head tag and destorys them as well
+
+// Usage:
+// var dev_var = envVar({
+// 	production:'myProductionURL',
+// 	development: 'myDevelopmentURL'
+// });
 var envVar = function(obj) {
-	if (process.env.NODE_ENV === 'development') {
-		return obj.development;
-
-	}
-	if (process.env.NODE_ENV === 'production') {
-		return obj.production;
-
+	for (var prop in obj) {
+		if (obj.hasOwnProperty(prop) && process.env.NODE_ENV === prop){
+			return obj[prop];	
+		}
 	}
 }
 module.exports = envVar;
