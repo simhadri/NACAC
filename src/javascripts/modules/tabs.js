3a09ff4d16e02926e039814b8315b359c71f75c8
jB <jeremy.bates@gmail.com>
2016-07-29 10:03:48 -0400

    adjustments to tabs & accordions

--- src/javascripts/modules/tabs.js
+++ src/javascripts/modules/tabs.js
@@ -1,14 +1,15 @@
 // FUNCTION
-(function() {
+(function tabsAndAccordions() {
 	'use strict';
 	
 	// a temp value to cache *what* we're about to show
 	var target = null;
 
 	// collect all the triggers
-	var triggers = $('.trigger__button').on('click', function() {
-		target = $(this.hash).removeAttr('id');
+	var triggers = $('.trigger__button');
 
+	function triggerClickHandler(){
+		target = $(this.hash).removeAttr('id');
 		$('.trigger__button').removeClass('trigger__button--active');
 		$(this).addClass('trigger__button--active');
 		// if the URL isn't going to change, then hashchange
@@ -18,7 +19,7 @@
 			// completed, so we wrap it in a setTimeout 0
 			setTimeout(update, 0);
 		}
-	});
+	}
 
 	// get an array of the panel ids (from the anchor hash)
 	var targets = triggers.map(function() {
@@ -27,12 +28,13 @@
 
 	function update() {
 
+		var hash = window.location.hash;
+
 		if (target) {
 			target.attr('id', target.data('old-id'));
 			target = null;
 		}
 
-		var hash = window.location.hash;
 		if (targets.indexOf(hash) !== -1) {
 			$('.trigger__button').removeClass('trigger__button--active');
 			$('a[href*="' + hash + '"]').addClass('trigger__button--active');
@@ -58,25 +60,32 @@
 		$('.trigger__content[data-trigger="' + id + '"]').addClass('trigger__content--active')
 	}
 
-	$(window).on('hashchange', update);
+	
 
 	// initialise
 	if (targets.indexOf(window.location.hash) !== -1) {
 		update();
-	} else {
+	}
+	else {
 		show();
 	}
-	// shadow animation
-	$('.trigger__navigation').scroll(
+	
 
-		function() {
-			var totalWidth = $('.trigger__button').length * $('.trigger__button').outerWidth(),
-				scrollLocation = $(this).scrollLeft() + $(this).width();
-			if (scrollLocation <= totalWidth - $('.trigger__button').outerWidth()) {
-				$('.trigger__block').removeClass('no--shadow');
-			} else {
-				$('.trigger__block').addClass('no--shadow');
-			}
+	function shadowAnimation(){
+		var triggerButton = $('.trigger__button');
+		var tabBlock = $('.tab__block');
+		var totalWidth = triggerButton.length * triggerButton.outerWidth();
+		var scrollLocation = $(this).scrollLeft() + $(this).width() + 10;
+
+		if (totalWidth >= scrollLocation) {
+			tabBlock.removeClass('no--shadow');
 		}
-	);
+		else {
+			tabBlock.addClass('no--shadow');
+		}
+	}
+
+	$(window).on('hashchange', update);
+	$('.tab__navigation').on('scroll', shadowAnimation);
+	triggers.on('click', triggerClickHandler);
 })();
