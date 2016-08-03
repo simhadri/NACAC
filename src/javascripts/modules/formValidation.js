ac7dcb976db273461096c1f577c2dc24da6532f9
Peter Grillot <petergrillot@gmail.com>
2016-08-03 11:41:09 -0400

    Add Registration Page

--- src/javascripts/modules/formValidation.js
+++ src/javascripts/modules/formValidation.js
@@ -16,21 +16,18 @@
 			display: 'email',
 			rules: 'required|valid_email'
 		}], function(errors, evt) {
-			// console.log(this.fields.lastName.element)
 			for (var field in this.fields) {
 			  if (this.fields[field].element.validity.valid){
 			  	this.fields[field].element.classList.remove('error');
 			  }else{
 			  	this.fields[field].element.classList.add('error');
 			  }
-			  // this.fields[field].isValid;
 			}
 			if (errors.length > 0) {
 				var errBox = document.getElementById('error-box');
 				errBox.innerHTML = '';
 				
 				for (var n = 0; n < errors[0].messages.length; n++) {
-					// errors[0].messages[n];
 					errBox.innerHTML = errBox.innerHTML + errors[0].messages[n] + '<br>';
 				}
 				errBox.classList.add('error-box--display')
