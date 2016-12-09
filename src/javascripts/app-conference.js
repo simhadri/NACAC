dba1cb433f7c1d741d692c57fd928c82768046ab
Peter Grillot <petergrillot@gmail.com>
2016-12-09 14:38:34 -0500

    Add Conference Utility

---
+++ src/javascripts/app-conference.js
@@ -0,0 +1,24 @@
+// *********************
+//    Modules scripts
+// *********************
+
+//USING production variables
+// if(process.env.NODE_ENV === 'development'){
+// 	var dev_var = 'url/to/dev';
+	
+// }
+// if(process.env.NODE_ENV === 'production'){
+// 	var dev_var = 'url/to/prod';
+	
+// }
+// console.log(dev_var)
+
+import 'modules/episerverViewMode';
+import 'modules/onScrolls';
+import 'modules/blazySettings';
+import 'modules/navigation';
+import 'modules/search';
+import 'modules/tabs';
+import 'modules/browser';
+import 'modules/tweetDeck';
+import 'modules/resizeAccordionImage';
